62e3bc411b52e2c1dbf4ea3a6996657f0a24908d
Jeff Leeder <jeffleeder@gmail.com>
2017-03-28 21:30:12 -0700

    Revert "login is loading as a js"
    This reverts commit 6b03edb87795daf8c602f444d9f12142cf47ae51.

--- lib/shopify_app/login_protection.rb
+++ lib/shopify_app/login_protection.rb
@@ -64,8 +64,16 @@ module ShopifyApp
     end
 
     def redirection_javascript(url)
-      %(     
-	      	  // If the current window is the 'parent', change the URL by setting location.href
+      %(
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8" />
+            <base target="_top">
+            <title>Redirecting…</title>
+            <script type="text/javascript">
+
+              // If the current window is the 'parent', change the URL by setting location.href
               if (window.top == window.self) {
                 window.top.location.href = #{url.to_json};
 
@@ -80,6 +88,12 @@ module ShopifyApp
                 });
                 window.parent.postMessage(data, "https://#{sanitized_shop_name}");
               }
+
+            </script>
+          </head>
+          <body>
+          </body>
+        </html>
       )
     end
 
